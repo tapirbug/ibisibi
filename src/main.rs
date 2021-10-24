8927bf7338cb0410d3323174f2e95641e92bbaf9
Philipp Stadler <hello@phstadler.com>
2021-10-24 13:12:58 +0200

    feat: add status query
    Add a function to query display status and extract parity calculation into its own module.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ mod args;
 mod cycle;
 mod destination;
 mod list;
+mod parity;
 mod plan;
 mod range;
 mod record;
 mod run;
 mod slot;
+mod status;
 mod telegram;
+mod serial;
 
 fn main() -> Result<(), String> {
     let args: args::TopLevel = argh::from_env();
