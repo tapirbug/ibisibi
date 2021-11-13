a3503b31aeb2d798f681d711e30063c6c9a6cb0c
krachzack <hello@phstadler.com>
2021-11-13 10:02:54 +0100

    feat: register tracing subscriber at startup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use tracing::Level;
+use tracing_subscriber::FmtSubscriber;
+
 mod args;
 mod cycle;
 mod destination;
@@ -15,6 +18,17 @@ mod telegram;
 mod serial;
 
 fn main() -> Result<(), String> {
+    // a builder for `FmtSubscriber`.
+    let subscriber = FmtSubscriber::builder()
+        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
+        // will be written to stdout.
+        .with_max_level(Level::TRACE)
+        // completes the builder.
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("setting default subscriber failed");
+
     let args: args::TopLevel = argh::from_env();
     run::run(args.invocation)
 }
